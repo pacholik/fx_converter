f065d9b70db12cb9bec22ea81918857c5a245dfb
Vojtěch Pachol <1668214+pacholik@users.noreply.github.com>
2021-01-11 16:34:08 +0100

    schema again from scratch

--- NOTES.md
+++ NOTES.md
@@ -7,3 +7,9 @@ First, download a lot of data at once to populate db.
 Do we have rates for the date in query?
  - Yes: search our db
  - No: download data from exchangeratesapi, populate db and return data.
+
+
+#Notes:
+
+I think the entire template schema is useless. I don't want direct db queries
+and I don't want any mutations.
--- fx_converter/schema.py
+++ fx_converter/schema.py
@@ -11,57 +11,47 @@ from fx_converter.database import db
 from fx_converter.models import Rate as RateModel
 
 
-class Rate(SQLAlchemyObjectType):
-    class Meta:
-        model = RateModel
-        interfaces = (relay.Node,)
+# class Rate(SQLAlchemyObjectType):
+#     class Meta:
+#         model = RateModel
+#         interfaces = (relay.Node,)
+
+
+class Rate(graphene.ObjectType):
+    from_ = graphene.String()
+    to_ = graphene.String()
+    day = graphene.Date()
 
 
 class Query(graphene.ObjectType):
-    node = relay.Node.Field()
-    all_items = SQLAlchemyConnectionField(Rate)
+    # node = relay.Node.Field()
+    # all_items = SQLAlchemyConnectionField(Rate)
+
+    # rates = graphene.List(Rate)
+    rate = graphene.Field(Rate,
+                          from_=graphene.String(),
+                          to_=graphene.String(),
+                          day=graphene.Date())
+
+    def resolve_rates(self, info, from_, to_, day):
+        print(self)
+        print(info)
+        print(from_)
+        print(to_)
+        print(day)
 
 
-class CreateItem(graphene.Mutation):
-    class Arguments:
-        key = graphene.String()
-        value = graphene.JSONString()
-
-    ok = graphene.Boolean()
-    item = graphene.Field(lambda: Rate)
-
-    def mutate(self, info, key, value):
-        item = RateModel(key=key, value=value)
-        db.session.add(item)
-        db.session.commit()
-        return CreateItem(ok=True, item=item)
+schema = graphene.Schema(query=Query)
 
 
-class DeleteItem(graphene.Mutation):
-    class Arguments:
-        id_ = graphene.Int(required=False)
-        key = graphene.String(required=False)
-
-    ok = graphene.Boolean()
-
-    def mutate(self, info, **kwargs):
-        item = None
-        if not kwargs:
-            raise Exception("Must provide either id_ or key")
-        if kwargs.get("_id"):
-            item = RateModel.get(kwargs["id_"])
-        else:
-            item = RateModel.get_by_key(kwargs["key"])
-
-        if not item:
-            raise Exception("Item not found")
-        item.delete()
-        return DeleteItem(ok=True)
-
-
-class Mutations(graphene.ObjectType):
-    create_item = CreateItem.Field()
-    delete_item = DeleteItem.Field()
-
-
-schema = graphene.Schema(query=Query, mutation=Mutations)
+if __name__ == '__main__':
+    print(
+        schema.execute('''
+            query {
+                rate(from_: "CZK", to_: "EUR", day: "today") {
+                    from_
+                    to_
+                    day
+                }
+            }''')
+    )
